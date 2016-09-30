5dc384d1ac90e6677f0bd83e7d2c6d43d96de007
arshitpatel <arshit.patel16@gmail.com>
2016-10-01 01:50:07 +0530

    Add files via upload

---
+++ debit_card.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<div class="container">
+<div class="embed-responsive embed-responsive-16by9" style="position:relative;">
+<img class="img-responsive" src="picture/debit.png" fixed/>
+<div class="embed-responsive-item" style="margin-top:17%; margin-left:6%; position:absolute;"><font size="6">0000 0000 0000 0000</font></div>
+<div class="embed-responsive-item" style="margin-top:22%; margin-left:22%; position:absolute;"><font  color="white" size="1">VALID</font></div>
+<div class="embed-responsive-item" style="margin-top:23%; margin-left:22%; position:absolute;"><font color="white" size="1">UPTO</font></div>
+<div class="embed-responsive-item" style="margin-top:22%; margin-left:26%; position:absolute;"><font color="white" size="3">02/19</font></div>
+<div class="embed-responsive-item" style="margin-top:22.5%; margin-left:3%; position:absolute;"><font color="white" size="2">ARSHIT KAJAVADARA</font></div>
+<div class="embed-responsive-item" style="margin-top:21.5%; margin-left:31%; position:absolute;"><img class="img-responsive" src="picture/mestro.png" height="3%" width="6%" fixed/></div>
+<div class="embed-responsive-item" style="margin-top:6%; margin-left:15%; position:absolute;  font-family: 'Times New Roman', Times, serif"><font color="white" size="5">BANK OF SURAT</font></div>
+<div class="embed-responsive-item" style="margin-top:3%; margin-left:2%; position:absolute;"><img class="img-responsive" src="picture/bos.png" height="10%" width="12%" fixed/></div>
+
+</div>
+
+
+
+</div>
+</div>
+
+
+
+<div class="container">
+<div class="embed-responsive embed-responsive-16by9" style="position:relative;">
+<img class="img-responsive" src="picture/cvv.png" style="border-radius:11px;" fixed/>
+<div class="embed-responsive-item" style="margin-top:8.43%; margin-left:19.2%; position:absolute;"><font color="red" size="2">345</font></div>
+
+
+
+</div>
+</div>
+</html>
---
+++ login_manager.php
@@ -0,0 +1,38 @@
+<html>
+<head><title>Manager Login Page</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+
+
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Manager Login</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">New account</a></li>
+      <li><a href="#">Requested Debitcard</a></li>
+      <li><a href="#">Delete account request</a></li>
+      <li><a href="#">Request Profile Update</a></li>
+      <li><a href="#">All for users</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Log Out</a></li>
+      </ul>
+  </div>
+</nav>
+  
+<h1><center><b><u><font size="14">BOS</h1></b></u><br>
+<center>Branch: SWAMI<br>
+<center><font size="10">Manager:----------</font></center>
+
+
+</body>
+</html>
